7bcf67b5de4210bda75bc2a3854e0a30cc5f8555
Zarin Loosli <zarinloosli@gmail.com>
2026-03-04 22:10:18 -0700

    Get file handle

--- src/pages/Home.mjs
+++ src/pages/Home.mjs
@@ -1,7 +1,6 @@
 import { LitElement, html, css } from "../libs/lit-all@2.7.6.js";
 import globalCss from "../global-styles/global.css.mjs";
-import { handleZipFile } from "../services/analysis/analysis.mjs";
-import { AsyncFileReader } from "../services/AysncFileReader.mjs";
+import "../services/analysis/analysis.mjs";
 
 export default class Home extends LitElement {
   static properties = {
@@ -12,39 +11,20 @@ export default class Home extends LitElement {
     super();
   }
 
-  /** @param {Event} event */
-  async onFileChange(event) {
-    const input = event.currentTarget;
-    if (!(input instanceof HTMLInputElement)) {
-      return;
-    }
+  async selectFile() {
+    const fileHandle = await showOpenFilePicker({});
+  }
 
-    const files = input.files ?? [];
-    const reader = new AsyncFileReader();
-    reader.addEventListener(
-      "progress",
-      (e) => (this.progress = (e.loaded / e.total) * 100)
-    );
-    try {
-      const buffer = await reader.readAsArrayBufferAsync(files[0]);
-    } catch (error) {
-      console.error(error);
-    }
+  async selectDirectory() {
+    const directoryHandle = await showDirectoryPicker();
     debugger;
-
-    handleZipFile(event);
   }
 
   render() {
     return html`<header><h1>hangouts-wrapped</h1></header>
       <main>Welcome to my app!</main>
-      <input
-        type="file"
-        name="file"
-        id="file-input"
-        @change="${this.onFileChange}"
-      />
-      Progress: ${this.progress}%`;
+      <button @click=${this.selectDirectory}>Select Directory</button>
+      <button @click=${this.selectFile}>Select file</button>`;
   }
 
   static styles = [globalCss, css``];
