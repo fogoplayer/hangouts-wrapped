edf1b12bfefff6583352feb69315f1201d7de5ce
Zarin Loosli <zarinloosli@gmail.com>
2026-03-07 00:47:43 -0700

    Send file byte arrays to a channel

--- src/services/analysis/src/model/channels.go
+++ src/services/analysis/src/model/channels.go
@@ -4,6 +4,8 @@ import (
 	"zarinloosli.com/hangouts-wrapped/model/jsonSchema"
 )
 
+var BytesChannel chan []byte = make(chan []byte)
+
 var UserInfoJsonChannel chan jsonSchema.UserInfo_JsonSchema = make(chan jsonSchema.UserInfo_JsonSchema)
 var GroupInfoJsonChannel chan jsonSchema.GroupInfo_JsonSchema = make(chan jsonSchema.GroupInfo_JsonSchema)
 var MessagesJsonChannel chan jsonSchema.Messages_JsonSchema = make(chan jsonSchema.Messages_JsonSchema)
