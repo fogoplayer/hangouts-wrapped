24000e45e36425189f659898dae56dfd021435a1
Zarin Loosli <zarinloosli@gmail.com>
2026-03-09 22:51:33 -0600

    Parse user_info.json

--- src/services/analysis/src/model/jsonSchema/userJsonSchema.go
+++ src/services/analysis/src/model/jsonSchema/userJsonSchema.go
@@ -1,19 +1,20 @@
 package jsonSchema
 
 type UserInfo_JsonSchema struct {
-	User                UserInfo_User_JsonSchema
-	Membership_Info     []UserInfo_MembershipInfo_JsonSchema
-	Starred_Group_Id    []string
-	Starred_Message_Ids []string
+	User                UserInfo_User_JsonSchema             `json:"user"`
+	Membership_Info     []UserInfo_MembershipInfo_JsonSchema `json:"membership_info"`
+	Starred_Group_Id    []string                             `json:"starred_group_id"`
+	Starred_Message_Ids []string                             `json:"starred_message_ids"`
 }
 
 type UserInfo_User_JsonSchema struct {
-	Name      string
-	Email     string
-	User_Type string
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	User_Type string `json:"user_type"`
 }
 
 type UserInfo_MembershipInfo_JsonSchema struct {
-	Group_Id         string
-	Membership_State string
+	Group_Name       string `json:"group_name"`
+	Group_Id         string `json:"group_id"`
+	Membership_State string `json:"membership_state"`
 }
--- src/services/analysis/src/parse/parse.go
+++ src/services/analysis/src/parse/parse.go
@@ -1,15 +1,32 @@
 package parse
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 
 	"zarinloosli.com/hangouts-wrapped/model"
+	"zarinloosli.com/hangouts-wrapped/model/jsonSchema"
 )
 
 func ParseChatDirectoryHandle(handle model.ChatDirectoryHandle) {
-	fmt.Println(handle.DirectoryHandle.Name())
+	// fmt.Println(handle.DirectoryHandle.Name())
 }
 
 func ParseUserInfo(bytes []byte) {
-	fmt.Println("Got user info")
+	userInfoJson := jsonSchema.UserInfo_JsonSchema{}
+	err := parseJson(bytes, &userInfoJson)
+	if err != nil {
+		fmt.Println("Error parsing user info:", err)
+	} else {
+		fmt.Println(userInfoJson)
+	}
+}
+
+func parseJson(bytes []byte, destinationPointer any) error {
+	if !json.Valid(bytes) {
+		return errors.New("invalid json")
+	}
+	json.Unmarshal(bytes, destinationPointer)
+	return nil
 }
