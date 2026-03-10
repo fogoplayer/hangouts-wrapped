a57dd0446292f8e511c83cc0b6297bc83d9f6dbb
Zarin Loosli <zarinloosli@gmail.com>
2026-03-09 20:15:05 -0600

    Add comments to primitives

--- src/services/analysis/src/browserApis/primitives_js.go
+++ src/services/analysis/src/browserApis/primitives_js.go
@@ -8,17 +8,6 @@ import (
 	"syscall/js"
 )
 
-func GetIntFromJsValue(jsValue js.Value) (int64, error) {
-	valueBytes := []byte{}
-	js.CopyBytesToGo(valueBytes, jsValue)
-
-	value, bytesRead := binary.Varint(valueBytes)
-	if bytesRead <= 0 {
-		return 0, errors.New("Couldn't read int")
-	}
-	return value, nil
-}
-
 func TypeMismatchError[T any](value js.Value) error {
 	return fmt.Errorf("%v cannot be coerced into %s", value, reflect.TypeFor[T]().Name())
 }
@@ -40,12 +29,31 @@ func SetGlobal(name string, x js.Value) {
 	fmt.Println("Set", name)
 }
 
+// ////////////////// //
+// GoFromJSConverters //
+// ////////////////// //
+
 func JsFromJsReturnValueUnchanged(v js.Value) (js.Value, error) {
 	return v, nil
 }
 
+// Converts as JS byte array into a Golang byte array
+//
+// Panics if value is of some other type, does not actually return an error.
+// However, it needs to match the signature of other *FromJs methods
 func ByteArrayFromJs(v js.Value) ([]byte, error) {
 	data := make([]byte, v.Length())
 	js.CopyBytesToGo(data, v)
-	return data, nil // TODO error handling for copyBytesToGo
+	return data, nil
+}
+
+func GetIntFromJsValue(jsValue js.Value) (int64, error) {
+	valueBytes := []byte{}
+	js.CopyBytesToGo(valueBytes, jsValue)
+
+	value, bytesRead := binary.Varint(valueBytes)
+	if bytesRead <= 0 {
+		return 0, errors.New("Couldn't read int")
+	}
+	return value, nil
 }
