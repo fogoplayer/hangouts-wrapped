8c11575fa135a6caa9d340fb953e4d2ba6262903
Zarin Loosli <zarinloosli@gmail.com>
2026-03-15 14:30:47 -0600

    Expose getIngestStats to JS

--- src/services/analysis/src/jsInterface/jsInterface_js.go
+++ src/services/analysis/src/jsInterface/jsInterface_js.go
@@ -3,14 +3,21 @@ package jsInterface
 import (
 	"syscall/js"
 
+	"zarinloosli.com/hangouts-wrapped/browserApis"
 	"zarinloosli.com/hangouts-wrapped/fsIo"
+	"zarinloosli.com/hangouts-wrapped/model"
 )
 
 func Initialize() {
 	js.Global().Set("showWasmDirectoryPicker", showDirectoryPicker)
+	js.Global().Set("getIngestStats", getIngestStats)
 }
 
 var showDirectoryPicker js.Func = js.FuncOf(func(this js.Value, args []js.Value) any {
 	fsIo.ShowDirectoryPicker()
 	return nil
+})
+
+var getIngestStats js.Func = js.FuncOf(func(this js.Value, args []js.Value) any {
+	return browserApis.ObjectFromGo(model.IngestStats)
 })
