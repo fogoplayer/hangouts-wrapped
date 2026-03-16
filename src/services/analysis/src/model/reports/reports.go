94562e92b6247e389be7fd0293601e04aa6a9a7e
Zarin Loosli <zarinloosli@gmail.com>
2026-03-15 23:04:12 -0600

    Create report enum

---
+++ src/services/analysis/src/model/reports/reports.go
@@ -0,0 +1,32 @@
+package model
+
+type report func(...[]any) any // TODO narrow this
+
+type reportName int
+
+const (
+	CountByPerson reportName = iota //Post count by person
+	// CountByDay reportName // Post count by day (month?)
+	CountByMonth
+	CountByYear
+	CountByHour
+	CountByChat        // Post count by chat (which is the biggest in the timeframe)
+	RandomMessage      // Get a random message in selection
+	RandomImage        // Get a random image in selection
+	MostUsedEmoji      // Emoji frequency?
+	MostReactions      // Most reacted-to message
+	MembershipTimeline // Every time someone joined and left one the selected chats in the selected timeframe
+	// Pipe dreams
+	WordFrequency // Google Books-style word frequency (Also check for common misspellings?)
+	// Line graphs of how often people were posting / emoji were used / words were used over time to see the ebb and flow
+	LongestThreads  // Longest thread
+	FirstMessages   // Each user's First message
+	WordCountByChat // Get the number of words that have been written
+	WordCountByUser
+)
+
+var Reports = map[reportName]report{}
+
+var ReportDescriptions = map[reportName]string{
+	CountByPerson: "Number of messages sent by each user",
+}
