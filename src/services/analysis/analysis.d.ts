63a321fc851d2582c0da7d4fc5179343ce63f1d8
Zarin Loosli <zarinloosli@gmail.com>
2026-03-18 21:02:14 -0600

    Allow retrieving application state from frontend

--- src/services/analysis/analysis.d.ts
+++ src/services/analysis/analysis.d.ts
@@ -4,9 +4,13 @@ declare global {
   interface Window {
     MyNamespace: any;
     documentJson: typeof documentJsonType; // used in analysis.mjs to expose JS function to Go
-    showWasmDirectoryPicker: () => void;
+    showWasmDirectoryPicker(): void;
     // It is VERY important that the return value is readonly
     // See IngestStatsType.String() for more details
     getIngestStats(): Readonly<Record<string, number>>;
+    getApplicationPhase(): Readonly<{
+      value: string; // actually an enum, but since it's readonly I think it's okay to be more general until we have codegen that can define TS enums from Go enums
+      onChange(callback: () => void): void;
+    }>;
   }
 }
--- src/services/analysis/src/jsInterface/jsInterface_js.go
+++ src/services/analysis/src/jsInterface/jsInterface_js.go
@@ -9,27 +9,52 @@ import (
 	"zarinloosli.com/hangouts-wrapped/util"
 )
 
+// The use of IIFEs in this file provide type safety. The return type of each function is documented in analysis.d.ts.
+// If a function return type is changed here, it should also be changed there
+
 func Initialize() {
 	js.Global().Set("showWasmDirectoryPicker", showDirectoryPicker)
 	js.Global().Set("getIngestStats", getIngestStats)
+	js.Global().Set("getApplicationPhase", getApplicationPhase)
 }
 
 var showDirectoryPicker js.Func = js.FuncOf(func(this js.Value, args []js.Value) any {
-	fsIo.ShowDirectoryPicker()
+	func() {
+		fsIo.ShowDirectoryPicker()
+	}()
 	return nil
 })
 
 var getIngestStats js.Func = js.FuncOf(func(this js.Value, args []js.Value) any {
-	convertToStringKeys := func(k state.IngestStatsKey, v int) (string, int) {
-		return string(k), v
-	}
+	jsReadyMap := func() map[string]int {
+		convertToStringKeys := func(k state.IngestStatsKey, v int) (string, int) {
+			return string(k), v
+		}
 
-	jsReadyMap := util.MapMap(state.GetIngestStats(), convertToStringKeys)
+		return util.MapMap(state.GetIngestStats(), convertToStringKeys)
+	}()
 	jsObject := browserApis.ObjectFromGo(jsReadyMap)
 	jsObject.Set("toString", js.FuncOf(func(this js.Value, args []js.Value) any {
 		// TODO this only works because we mark the return value as Readonly
-		// Might be worth using code gen to get a more robus solution
+		// Might be worth using code gen to get a more robust solution
 		return js.ValueOf(state.GetIngestStats().String())
 	}))
 	return jsObject
+})
+
+var getApplicationPhase js.Func = js.FuncOf(func(this js.Value, args []js.Value) any {
+	return js.ValueOf(map[string]any{
+		"value": string(state.ApplicationPhase.Value()),
+		"onChange": js.FuncOf(func(this js.Value, args []js.Value) any {
+			for _, arg := range args {
+				isFunction := arg.InstanceOf(js.Global().Get("Function"))
+				if isFunction {
+					state.ApplicationPhase.OnChange(func() {
+						arg.Call("call")
+					})
+				}
+			}
+			return nil
+		}),
+	})
 })
