03f1225cc5b2ce2ea49ec8120f9edc26fae9a2b9
Zarin Loosli <zarinloosli@gmail.com>
2026-04-14 23:09:15 -0600

    Add semi-dynamic sizing to charts--still some work to do

--- src/components/Chart.mjs
+++ src/components/Chart.mjs
@@ -4,10 +4,14 @@ import { createRef, html, LitElement, ref } from "../libs/lit-all@2.7.6.js";
 export class ChartComponent extends LitElement {
   static properties = {
     config: { type: Object },
+    outputLineCount: { type: Number },
   };
 
+  /** @type {number} */
+  outputLineCount = 0;
   /** @type {import("../libs/chart@4.5.0.js").ChartConfiguration | undefined} */
   config = undefined;
+  /** @type {import("../libs/lit-all@2.7.6.js").Ref<HTMLCanvasElement>} */
   canvasRef = createRef();
 
   firstUpdated() {
@@ -22,13 +26,20 @@ export class ChartComponent extends LitElement {
   }
 
   updateChart() {
-    if (this.config) {
-      (async () => new Chart(this.canvasRef.value, this.config))();
+    if (this.config && this.canvasRef.value) {
+      this.outputLineCount = this.config.data.labels?.length ?? 0;
+      // I don't fully understand why this is is needed in addition to the height, but it is
+      this.canvasRef.value.style.height = this.outputLineCount + "em";
+      console.log(this.config);
+      (async () => new Chart(this.canvasRef.value, this.config))(); // TODO fix type
     }
   }
 
   render() {
-    return html`<canvas ${ref(this.canvasRef)}></canvas>`;
+    return html`<canvas
+      ${ref(this.canvasRef)}
+      height=${this.outputLineCount * 32}
+    ></canvas>`;
   }
 }
 
