1b11c16708397df7930307ff29679e7ea74438b8
Zarin Loosli <zarinloosli@gmail.com>
2026-04-24 18:27:23 -0600

    Use scanner instead of reader

--- src/services/analysis/src/userInteractionIo/userInteractionIo_native.go
+++ src/services/analysis/src/userInteractionIo/userInteractionIo_native.go
@@ -7,18 +7,17 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
 
 func Prompt(message string, options []string) int {
-	reader := bufio.NewReader(os.Stdin) // TODO 	bufio.NewScanner()
+	reader := bufio.NewScanner(os.Stdin)
 	fmt.Println(message)
 	for {
 		for i, option := range options {
 			fmt.Printf("  (%d) %s\n", i, option)
 		}
-		text, _ := reader.ReadString('\n')
-		text = strings.ReplaceAll(text, "\n", "")
+		reader.Scan()
+		text := reader.Text()
 		selection, err := strconv.Atoi(text)
 		if err == nil && selection >= 0 && selection < len(options) {
 			return selection
